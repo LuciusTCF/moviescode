e5ac0b5552ebd081ff869c1ac0e5b1244471f437
Maximo Villalba <villalbamaxi1025@gmail.com>
2023-09-11 18:19:47 -0300

    subida de index

---
+++ js/data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+/// and so on
+]
+/// video cards
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    })
+})
